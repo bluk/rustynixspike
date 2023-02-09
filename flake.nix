3f7b3a8bd34c8540d7eafb926370100a383a719b
Bryant Luk <code@bryantluk.com>
2023-02-09 17:28:39 +0000

    nixfmt

--- flake.nix
+++ flake.nix
@@ -7,73 +7,81 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  inputs.import-cargo.url = github:edolstra/import-cargo;
+  inputs.import-cargo.url = "github:edolstra/import-cargo";
 
   outputs = { self, nixpkgs, import-cargo }:
     let
 
       # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      lastModifiedDate =
+        self.lastModifiedDate or self.lastModified or "19700101";
 
       # Generate a user-friendly version number.
-      version = "${builtins.substring 0 8 lastModifiedDate}-${self.shortRev or "dirty"}";
+      version = "${builtins.substring 0 8 lastModifiedDate}-${
+          self.shortRev or "dirty"
+        }";
 
       # System types to support.
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        });
 
     in {
 
       # A Nixpkgs overlay.
       overlays.default = final: prev: {
 
-        rustynixspike = with final; final.callPackage ({ inShell ? false }: final.rustPlatform.buildRustPackage rec {
-          pname = "rustynixspike-${version}";
-		version = "0.1.0";
+        rustynixspike = with final;
+          final.callPackage ({ inShell ? false }:
+            final.rustPlatform.buildRustPackage rec {
+              pname = "rustynixspike-${version}";
+              version = "0.1.0";
 
-          # In 'nix develop', we don't need a copy of the source tree
-          # in the Nix store.
-          # src = if inShell then null else ./.;
-          src = ./.;
+              # In 'nix develop', we don't need a copy of the source tree
+              # in the Nix store.
+              # src = if inShell then null else ./.;
+              src = ./.;
 
-          cargoSha256 = "sha256-odcOQu12jzHrMit2nnQubAhg7pb50jNGtcw0/Iyda0s=";
-        }) {};
+              cargoSha256 =
+                "sha256-odcOQu12jzHrMit2nnQubAhg7pb50jNGtcw0/Iyda0s=";
+            }) { };
 
       };
 
       # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) rustynixspike;
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}) rustynixspike;
 
-	  # The default package for 'nix build'. This makes sense if the
-	  # flake provides only one package or there is a clear "main"
-	  # package.
-          default = self.packages.${system}.rustynixspike;
-        });
+        # The default package for 'nix build'. This makes sense if the
+        # flake provides only one package or there is a clear "main"
+        # package.
+        default = self.packages.${system}.rustynixspike;
+      });
 
       # Provide a 'nix develop' environment for interactive hacking.
       devShells = forAllSystems (system: {
-default = self.packages.${system}.rustynixspike.override { inShell = true; };
-}
-);
+        default =
+          self.packages.${system}.rustynixspike.override { inShell = true; };
+      });
 
       # A NixOS module.
-      nixosModules.rustynixspike =
-        { pkgs, ... }:
-        {
-          nixpkgs.overlays = [ self.overlays.default ];
+      nixosModules.rustynixspike = { pkgs, ... }: {
+        nixpkgs.overlays = [ self.overlays.default ];
 
-          systemd.services.rustynixspike = {
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig.ExecStart = "${pkgs.rustynixspike}/bin/rustynixspike";
-          };
+        systemd.services.rustynixspike = {
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig.ExecStart = "${pkgs.rustynixspike}/bin/rustynixspike";
         };
+      };
 
       # Tests run by 'nix flake check' and by Hydra.
       #checks = forAllSystems
