e03c6d199c45f4f7b25085bc537d112032829620
Bryant Luk <code@bryantluk.com>
2023-02-09 17:26:17 +0000

    Use rustPlatform.buildRustPackage instead of stdenv.mkDerivation

--- flake.nix
+++ flake.nix
@@ -32,41 +32,16 @@
       # A Nixpkgs overlay.
       overlays.default = final: prev: {
 
-        rustynixspike = with final; final.callPackage ({ inShell ? false }: stdenv.mkDerivation rec {
-          name = "rustynixspike-${version}";
+        rustynixspike = with final; final.callPackage ({ inShell ? false }: final.rustPlatform.buildRustPackage rec {
+          pname = "rustynixspike-${version}";
+		version = "0.1.0";
 
           # In 'nix develop', we don't need a copy of the source tree
           # in the Nix store.
-          src = if inShell then null else ./.;
+          # src = if inShell then null else ./.;
+          src = ./.;
 
-          buildInputs =
-            [ rustc
-              cargo
-            ] ++ (if inShell then [
-              # In 'nix develop', provide some developer tools.
-              rustfmt
-              clippy
-            ] else [
-              (import-cargo.builders.importCargo {
-                lockFile = ./Cargo.lock;
-                inherit pkgs;
-              }).cargoHome
-            ]);
-
-          target = "--release";
-
-          buildPhase = "cargo build ${target} --frozen --offline";
-
-          doCheck = true;
-
-          checkPhase = "cargo test ${target} --frozen --offline";
-
-          installPhase =
-            ''
-              mkdir -p $out
-              cargo install --frozen --offline --path . --root $out
-              rm $out/.crates.toml
-            '';
+          cargoSha256 = "sha256-odcOQu12jzHrMit2nnQubAhg7pb50jNGtcw0/Iyda0s=";
         }) {};
 
       };
